8ab57121f12ab111c6393a4aff16f4f9f356955a
Attalurimaheedhar <maheedharattaluri9@gmail.com>
2025-03-16 08:24:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def predict():
         return render_template('recomend.html', prediction_text=f"Error: {str(e)}")
 
 if __name__ == '__main__':
+    port =int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0',port=5000,debug=True)
